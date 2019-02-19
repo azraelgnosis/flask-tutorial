b4d2b74bf479c4e9093760850703b39b6c2b9f37
azraelgnosis <azraelgnosis@gmail.com>
2019-02-19 02:00:15 -0500

    Add blog\index template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -4,7 +4,7 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
-from flask.db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
